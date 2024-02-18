bdbf96f67bf75bcedf6213103b5184f280e2b788
gharby.moez <gharby.moez@outlook.fr>
2024-02-19 00:34:19 +0100

    Catalog + Basket

---
+++ src/Services/Catalog/Catalog.API/Migrations/20240218161953_initdb.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catalog.API.Migrations
+{
+    public partial class initdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    Summary = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageFile = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "ImageFile", "Name", "Price", "Summary" },
+                values: new object[] { "602d2149e773f2a3990b47f5", "Smart Phone", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.", "product-1.png", "IPhone X", 950.00m, "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless." });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "ImageFile", "Name", "Price", "Summary" },
+                values: new object[] { "602d2149e773f2a3990b47f6", "Smart Phone", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.", "product-2.png", "Samsung 10", 840.00m, "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless." });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "ImageFile", "Name", "Price", "Summary" },
+                values: new object[] { "602d2149e773f2a3990b47f7", "White Appliances", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.", "product-3.png", "Huawei Plus", 650.00m, "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless." });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "ImageFile", "Name", "Price", "Summary" },
+                values: new object[] { "602d2149e773f2a3990b47f8", "White Appliances", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.", "product-4.png", "Xiaomi Mi 9", 470.00m, "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless." });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "ImageFile", "Name", "Price", "Summary" },
+                values: new object[] { "602d2149e773f2a3990b47f9", "Smart Phone", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.", "product-5.png", "HTC U11+ Plus", 380.00m, "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless." });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "ImageFile", "Name", "Price", "Summary" },
+                values: new object[] { "602d2149e773f2a3990b47fa", "Home Kitchen", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.", "product-6.png", "LG G7 ThinQ", 240.00m, "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless." });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,87 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DataAppContext _context;
+
+        public ProductRepository(DataAppContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context
+                            .Products.ToListAsync();
+
+
+        }
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context
+                           .Products
+
+                           .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+
+
+            return await _context
+                            .Products
+                            .Where(x => x.Name == name)
+                            .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+
+
+            return await _context
+                            .Products
+                            .Where(x => x.Category == categoryName)
+                            .ToListAsync();
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _context
+                                        .Products.FindAsync(product.Id);
+            updateResult.Name = product.Name;
+            updateResult.Description = product.Description;
+            updateResult.Category = product.Category;
+
+            return true;
+
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var updateResult = await _context
+                                        .Products.FindAsync(id);
+
+            _context.Products.Remove(updateResult);
+            await _context.SaveChangesAsync();
+            return true;
+
+
+
+
+        }
+
+    }
+}
