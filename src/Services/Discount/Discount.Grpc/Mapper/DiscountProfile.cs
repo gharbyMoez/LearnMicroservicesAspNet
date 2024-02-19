64c4375f265eeea313abd428000ef55f1aac3d24
gharby.moez <gharby.moez@outlook.fr>
2024-02-19 22:57:13 +0100

    Basket + Discount grpc services

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+
+namespace Discount.Grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            /*CreateMap<Coupon, CouponModel>().ReverseMap();*/
+        }
+    }
+}
